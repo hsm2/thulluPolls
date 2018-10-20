a8e5eff6b4fb242f9b19a87fbf1c9f499a66da76
Surabhi Jain <surabhijain@Surabhis-MBP-2.lan>
2018-10-20 15:48:04 -0500

    hello

--- signup.php
+++ signup.php
@@ -1,13 +1,36 @@
-<html>
-<head><meta http-equiv="Content-Type" content="text/html; charset=us-ascii">
-	<title></title>
-	<link rel="stylesheet" href="w3.css">
-</head>
+<?php
+session_start();
+$_SESSION['message'] = '';
+$mysqli = new mysqli("cpanel3.engr.illinois.edu", "thullupolls_thullu", "Thullu123!", "thullupolls_thullupolls");
+#include('bankinfo.php');
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if($_POST['password'] == $_POST['confirmpassword'] && ($_POST['bank_password'] == $_POST['bank_confirmpassword'])){
+        $fullname = $mysqli->real_escape_string($_POST['fullname']);
+        $username = $mysqli->real_escape_string($_POST['username']);
+        $bank_username = $mysqli->real_escape_string($_POST['bank_username']);
+        $bank_password = $mysqli->real_escape_string($_POST['bank_password']);
+        $password = $_POST['password'];
+        $bank_type = $_POST['bank_type'];
+        #$bankID = 0;
+        $_SESSION['fullname'] = $fullname;
+        $_SESSION['username'] = $username;
+        $balance = 0;
+        $sql = "INSERT INTO User (username, name, password, balance, bankID ) " . "VALUES ('$username', '$fullname', '$password', '$balance', '$bank_type')";
+        $sql1 = "INSERT INTO Bank (bankID, crypto_username, bank_username, password) " . "VALUES ('$bank_type', '$username', '$bank_username', '$bank_password')";
 
-<h1 style="background-color:#f1f1f1">
-  Thullu Polls
-  <div class="imgcontainer">
-    <img class = "resize" src="thullu.png" alt="Avatar" class="avatar">
-  </div>
-</h1>
-</html>
+        if(($mysqli->query($sql) === true) && ($mysqli->query($sql1) === true)){
+            $_SESSION['message'] = "Registration Successful! $username has been added to the database!";
+            header("location: index.html");
+            #header("location:bankinfo.php");
+        }
+        else{
+            $_SESSION['message'] = "User could not be added to database";
+        }
+
+    }
+    else{
+        $_SESSION['message'] = "Two passwords do not match!";
+    }
+}
+$mysqli->close();
+?>
