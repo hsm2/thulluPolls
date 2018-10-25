da2c3826c98f937c94ec5961274e0ff1520b526c
hsm2 <hsm2@illinois.edu>
2018-10-25 15:12:19 -0500

    hello

--- signup.php
+++ signup.php
@@ -8,12 +8,25 @@
 session_start();
 $_SESSION['message'] = '';
 $mysqli = new mysqli("cpanel3.engr.illinois.edu", "thullupolls_thullu", "Thullu123!", "thullupolls_thullupolls");
+$link = mysqli_connect("cpanel3.engr.illinois.edu", "thullupolls_thullu", "Thullu123!", "thullupolls_thullupolls");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
 #include('bankinfo.php');
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     if($_POST['password'] == $_POST['confirmpassword']){
-        $name = $_POST['name'];
-        $id = $_POST['id'];
-        $password = $_POST['password'];
+        $name = $mysqli->real_escape_string($_POST['name']);
+        $id = $mysqli->real_escape_string($_POST['id']);
+        $password = $mysqli->real_escape_string($_POST['password']);
         $_SESSION['name'] = $name;
         $_SESSION['id'] = $id;
         $sql = "INSERT INTO User (id, name, password) " . "VALUES ('$id', '$name', '$password')";
