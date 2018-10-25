dac8acac71d5cf5c3b8c0ed9931f0386f6a6b107
Surabhi Jain <surabhijain@wirelessprv-10-194-91-233.near.illinois.edu>
2018-10-25 16:15:15 -0500

    hello

--- signup.php
+++ signup.php
@@ -32,7 +32,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $sql = "INSERT INTO User (id, name, password) " . "VALUES ('$id', '$name', '$password')";
 
         if(($mysqli->query($sql) === true)){
-						header("location: signin.php");
+						header("Location: signin.php");
             $_SESSION['message'] = "Registration Successful! Welcome $id";
         }
         else{
