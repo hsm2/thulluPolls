5a385b91a3f01efff028df74f1de7743fa3d67d4
hsm2 <hsm2@illinois.edu>
2018-10-25 18:09:31 -0500

    h

--- signin.php
+++ signin.php
@@ -16,7 +16,6 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$_SESSION['password'] = $password;
 	$sql = "SELECT * FROM User WHERE (id = '$username' AND password = '$password')";
 	$result = $mysqli->query($sql);
-	$mysqli->close();
 	if ($result->num_rows > 0) {
 		$_SESSION['message'] = "You're signed in!";
 		header("Location:homepage.php");
@@ -25,6 +24,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 		$_SESSION['message'] = "You fucked up";
 	}
 }
+$mysqli->close();
 ?>
 
 <html>
