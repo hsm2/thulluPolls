e6ff13c222efc7543ed3345e25b80ebcbaecda8a
Surabhi Jain <surabhijain@wirelessprv-10-194-91-233.near.illinois.edu>
2018-10-31 14:56:22 -0500

    hello

--- demo.php
+++ demo.php
@@ -117,8 +117,8 @@ $mysqli->close();
 						      <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
 						      <input style="width: 300px;  padding-right: 20px; border: 3px solid #555;" type="text" placeholder="Poll Name" name="name" required /> <br><br>
 						      Question <input style="width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Question" name="q1" /> <br><br>
-						      Answer Choice 1 <br> <input style="width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Answer Choice 1" name="a1"/>
-						      Answer Choice 2 <br> <input style="width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Answer Choice 2" name="a2"/>
+						      Answer Choice 1 <br> <input style="width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Answer Choice 1" name="a1"/><br><br>
+						      Answer Choice 2 <br> <input style="width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Answer Choice 2" name="a2"/><br><br>
                   Answer Choice 3 <br> <input style="width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Answer Choice 3" name="a3"/> <br><br>
                   <input type="checkbox" name="Public" value="public"> Public Post <br><br><br>
 
