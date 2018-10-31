de8433a21cbff1a72277178e21bf09b51116cae4
Surabhi Jain <surabhijain@wirelessprv-10-194-91-233.near.illinois.edu>
2018-10-31 16:03:02 -0500

    hello

--- demo.php
+++ demo.php
@@ -29,13 +29,23 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$sql4 = "INSERT INTO Options (option_num, option_name, poll_id, total_votes)"
 					. "VALUES (3, '$answer3', '$poll_id', 0)";
 
-	if ($mysqli->query($sql1) == false) {
+	if ($mysqli->query($sql1) == false ) {
 		$_SESSION['message'] = "Problem 1";
 		echo "Problem 1";
 	}
-	else {
-		header("Location:demo2.php")
+	if ($mysqli->query($sql2) == false) {
+		$_SESSION['message'] = "Problem 2";
+		echo "Problem 1";
 	}
+	if ($mysqli->query($sql3) == false) {
+		$_SESSION['message'] = "Problem 3";
+		echo "Problem 1";
+	}
+	if ($mysqli->query($sql4) == false) {
+		$_SESSION['message'] = "Problem 4";
+		echo "Problem 4";
+	}
+
 
     // if($_POST['password'] == $_POST['confirmpassword']){
     //     $name = $mysqli->real_escape_string($_POST['name']);
