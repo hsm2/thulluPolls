3986fa313679cfe6dc616dd705ae865983618dc6
Surabhi Jain <surabhijain@Surabhis-MacBook-Pro-2.local>
2018-11-23 17:07:37 -0600

    hello

--- view_polls.php
+++ view_polls.php
@@ -24,7 +24,7 @@ $result = $mysqli->query($sql);
 if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
     ?>
-    <p> Name : <?php echo $row[0] ?> </p>
+    <p> Name : <?php echo $row['poll_name'] ?> </p>
     <?php
   }
 }
