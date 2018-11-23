73ebd69b10ab95aaed371a80310a052134d4f105
Surabhi Jain <surabhijain@Surabhis-MacBook-Pro-2.local>
2018-11-23 17:06:48 -0600

    hello

--- view_polls.php
+++ view_polls.php
@@ -16,26 +16,16 @@ echo $_SESSION['username']
 <?php
 $_SESSION['message'] = '';
 $mysqli = new mysqli("127.0.0.1", "thullupolls_root", "Surabhiharish", "thullupolls_thullupolls");
-?>
-<p> Hello1 : </p>
-<?php
 
 $owner = $_SESSION['username'];
 $sql = "SELECT poll_name FROM Poll WHERE owner = '$owner'";
-
-?>
-<p> Hello : </p>
-<?php
+$result = $mysqli->query($sql);
 
 if ($result->num_rows > 0) {
-  // Show each data returned by mysql
   while($row = $result->fetch_assoc()) {
-  ?>
-
-  <!-- USING HTML HERE : Here I am using php within html tags -->
-  <p> Name : <?php echo $row ?> </p>
-
-<?php
+    ?>
+    <p> Name : <?php echo $row ?> </p>
+    <?php
   }
 }
 else {
