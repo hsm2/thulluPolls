2adad6605233d86802cd4e0cdce92d3abb875096
Surabhi Jain <surabhijain@Surabhis-MacBook-Pro-2.local>
2018-11-23 16:53:20 -0600

    hello

---
+++ view_polls.php
@@ -0,0 +1,131 @@
+<?php
+ ob_start();  //begin buffering the output
+?>
+
+<?php
+session_start();
+echo $_SESSION['username']
+?>
+
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=us-ascii">
+	<title></title>
+	<link rel="stylesheet" href="w3.css">
+</head>
+
+<?php
+$_SESSION['message'] = '';
+$mysqli = new mysqli("127.0.0.1", "thullupolls_root", "Surabhiharish", "thullupolls_thullupolls");
+$owner = $_SESSION['username'];
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+  $sql = "SELECT poll_name FROM Poll WHERE owner = '$owner'";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // Show each data returned by mysql
+    while($row = $result->fetch_assoc()) {
+    ?>
+
+  	<!-- USING HTML HERE : Here I am using php within html tags -->
+  	<p> Name : <?php echo $row ?> </p>
+
+  <?php
+    }
+  }
+  else {
+    echo "0 results";
+  }
+
+  }
+?>
+
+
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Thullu Polls &mdash; Share Your Opinions</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by gettemplates.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="gettemplates.co" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400|Montserrat:400,700" rel="stylesheet"> -->
+
+	<link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Themify Icons-->
+	<link rel="stylesheet" href="css/themify-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+	<div class="gtco-loader"></div>
+
+	<div id="page">
+
+		<nav class="gtco-nav" role="navigation">
+			<div class="container">
+
+				<div class="row">
+					<div class="col-xs-10 text-right fh5co-top-social">
+						<ul class="gtco-social">
+							<li><a href="#" class="icon-twitter"></i></a></li>
+							<li><a href="#" class="icon-dribbble"></i></a></li>
+							<li><a href="#" class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+
+			</div>
+		</nav>
+		
+
+
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
