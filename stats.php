9e9fc12bf7d5b6bb9ad0edcbf0963ccfa2807b69
Surabhi Jain <surabhijain@Surabhis-MacBook-Pro-2.local>
2018-11-24 16:48:17 -0600

    hello

--- stats.php
+++ stats.php
@@ -5,29 +5,30 @@
       google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
 
+      var mysql = require('mysql');
+
+      var con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "thullupolls_root",
+        password: "Surabhiharish",
+        database: "thullupolls_thullupolls"
+      });
+
+      con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+      });
+
+      con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT * FROM Options", function (err, result, fields) {
+          if (err) throw err;
+          console.log(result);
+        });
+      });
+
+
       function drawChart() {
-        // var mysql = require('mysql');
-        //
-        // var con = mysql.createConnection({
-        //   host: "127.0.0.1",
-        //   user: "thullupolls_root",
-        //   password: "Surabhiharish",
-        //   database: "thullupolls_thullupolls"
-        // });
-
-        // con.connect(function(err) {
-        //   if (err) throw err;
-        //   console.log("Connected!");
-        // });
-        //
-        // con.connect(function(err) {
-        //   if (err) throw err;
-        //   con.query("SELECT * FROM Options", function (err, result, fields) {
-        //     if (err) throw err;
-        //     console.log(result);
-        //   });
-        // });
-
         var data = google.visualization.arrayToDataTable([
           ['Task', 'Hours per Day'],
           ['Work',     11],
