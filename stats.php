e03aaea31a617da16ce9ed4cc13abc34245cb493
hsm2 <hsm2@illinois.edu>
2018-11-25 20:24:08 -0500

    hey

--- stats.php
+++ stats.php
@@ -1,7 +1,7 @@
 <html>
   <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
+    <script type="text/javascript" src="require.js">
       google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
 
