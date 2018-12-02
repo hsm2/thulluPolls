907b05f23d59d5fd4bd657636c3e4fcef93b3a23
sjain0721 <surabhijain0721@gmail.com>
2018-12-01 19:41:59 -0600

    hello

--- register.php
+++ register.php
@@ -3,22 +3,9 @@
 session_start();
 $_SESSION['message'] = '';
 $mysqli = new mysqli("127.0.0.1", "thullupolls_root", "Surabhiharish", "thullupolls_thullupolls");
-$link = mysqli_connect("127.0.0.1", "thullupolls_root", "Surabhiharish", "thullupolls_thullupolls");
 
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-mysqli_close($link);
-
-echo $_SERVER['REQUEST_METHOD'];
 echo 'no';
+echo $_SERVER['REQUEST_METHOD'];
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
   echo "Im here";
