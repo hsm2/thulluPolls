c13307a3459e103d6044b0445dfc646d82d48d36
sjain0721 <surabhijain0721@gmail.com>
2018-12-01 19:19:23 -0600

    hello

--- login.php
+++ login.php
@@ -19,6 +19,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	if ($result->num_rows > 0) {
 		$_SESSION['message'] = "You're signed in!";
 		header("Location:welcome.php");
+    echo "hello";
 		ob_flush();
 	}
 	else {
@@ -60,7 +61,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-50">
-				<form class="login100-form validate-form" action="login.php" method="post">
+				<form class="login100-form validate-form" action="welcome.php" method="post">
 					<span class="login100-form-title p-b-33">
 						Account Login
 					</span>
