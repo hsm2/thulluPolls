cab471d2db101b3a025062d5adbe08e66e0f22b5
sjain0721 <surabhijain0721@gmail.com>
2018-12-02 13:59:01 -0600

    hello

--- create.php
+++ create.php
@@ -7,12 +7,6 @@ session_start();
 echo $_SESSION['username']
 ?>
 
-<html>
-<head><meta http-equiv="Content-Type" content="text/html; charset=us-ascii">
-	<title></title>
-	<link rel="stylesheet" href="w3.css">
-</head>
-
 <?php
 $_SESSION['message'] = '';
 $mysqli = new mysqli("127.0.0.1", "thullupolls_root", "Surabhiharish", "thullupolls_thullupolls");
@@ -78,121 +72,111 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 ?>
 
 
-<html>
-	<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Thullu Polls &mdash; Share Your Opinions</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Free HTML5 Website Template by gettemplates.co" />
-	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
-	<meta name="author" content="gettemplates.co" />
+<html lang="">
+<head>
+<title>Thullu Polls | Pages | View Polls</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="../layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row1">
+  <header id="header" class="hoc clear">
+    <!-- ################################################################################################ -->
+    <div id="logo" class="fl_left">
+      <h1><a href="index.html">Thullu Polls</a></h1>
+    </div>
+    <nav id="mainav" class="fl_right">
+      <ul class="clear">
+        <li class="active"><a href="index.html">Home</a></li>
+        <li><a class="drop" href="#">Pages</a>
+          <ul>
+            <li><a href="participate.php">Participate in Polls</a></li>
+            <li><a href="view_polls.php">View Your Polls</a></li>
+            <li><a href="create.php">Create Polls</a></li>
+          </ul>
+        </li>
+        <li><a href="view_polls.php"><?php echo $_SESSION['username'] ?></a></li>
+        <li><a href="index.html" >Logout</a></li>
+      </ul>
+    </nav>
+    <!-- ################################################################################################ -->
+  </header>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row2">
+  <div id="breadcrumb" class="hoc clear">
+    <!-- ################################################################################################ -->
+    <ul>
+      <li><a href="welcome.php">Home</a></li>
+      <li><a href="view_polls.php">View Your Polls</a></li>
+    </ul>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <main class="hoc container clear">
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <div class="content">
+      <!-- ################################################################################################ -->
+      <img class="imgr borderedbox inspace-5" src="../images/demo/imgr.gif" alt="">
+      <div id="comments">
+        <h2>Your Polls</h2>
 
-  	<!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
+        <ul>
 
-	<!-- <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400|Montserrat:400,700" rel="stylesheet"> -->
+          <li>
+            <article>
+              <header>
+                </header>
+                <figure class="avatar"><img src="../images/demo/avatar.png" alt=""></figure>
+                <address>
+                  <div class="comcont">
+                    <form class="form"  method="post" enctype="multipart/form-data" autocomplete="off">
+    						      <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
+    						      <input style="width: 300px;  padding-right: 20px; border: 3px solid #555;" type="text" placeholder="Poll Name" name="name" required /> <br><br>
+    						      Question <br><input style=" width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Question" name="q1" /> <br><br>
+    						      Answer Choice 1 <br> <input style=" width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Answer Choice 1" name="a1"/><br><br>
+    						      Answer Choice 2 <br> <input style=" width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Answer Choice 2" name="a2"/><br><br>
+                      Answer Choice 3 <br> <input style=" width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Answer Choice 3" name="a3"/> <br><br>
+                      Answer Choice 4 <br> <input style=" width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Answer Choice 4" name="a4"/> <br><br>
+                      <input type="checkbox" name="Public" value="public"> Public Post <br><br><br>
 
-	<link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet">
+    						      <input type="submit" value="Create Poll" name="Create Poll" class="btn btn-block btn-primary" />
+    						      <div class="module">
+    						    </form>
 
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="css/animate.css">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="css/icomoon.css">
-	<!-- Themify Icons-->
-	<link rel="stylesheet" href="css/themify-icons.css">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<!-- Theme style  -->
-	<link rel="stylesheet" href="css/style.css">
+                  </div>
+                </address>
+            </article>
 
-	<!-- Modernizr JS -->
-	<script src="js/modernizr-2.6.2.min.js"></script>
-	<!-- FOR IE9 below -->
-	<!--[if lt IE 9]>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
+          </li>
 
-	</head>
-	<body>
-
-	<div class="gtco-loader"></div>
-
-	<div id="page">
-
-		<nav class="gtco-nav" role="navigation">
-			<div class="container">
-
-				<div class="row">
-					<div class="col-xs-10 text-right fh5co-top-social">
-						<ul class="gtco-social">
-							<li><a href="#" class="icon-twitter"></i></a></li>
-							<li><a href="#" class="icon-dribbble"></i></a></li>
-							<li><a href="#" class="icon-instagram"></i></a></li>
-						</ul>
-					</div>
-				</div>
-
-			</div>
-		</nav>
-		<div id="gtco-intro">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-10 col-md-offset-1 text-center">
-						<div class="dt js-height">
-							<div class="dtc animate-box">
-                <a class="topnav" href="welcome.php" title="Homepage">Home</a>
-								<h2 class="gradient-text">Create a Poll</h2>
-								<form class="form"  method="post" enctype="multipart/form-data" autocomplete="off">
-						      <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
-						      <input style="width: 300px;  padding-right: 20px; border: 3px solid #555;" type="text" placeholder="Poll Name" name="name" required /> <br><br>
-						      Question <br><input style=" width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Question" name="q1" /> <br><br>
-						      Answer Choice 1 <br> <input style=" width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Answer Choice 1" name="a1"/><br><br>
-						      Answer Choice 2 <br> <input style=" width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Answer Choice 2" name="a2"/><br><br>
-                  Answer Choice 3 <br> <input style=" width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Answer Choice 3" name="a3"/> <br><br>
-                  Answer Choice 4 <br> <input style=" width: 300px; height: 50px; border: 3px solid #555;" type="text" placeholder="Answer Choice 4" name="a4"/> <br><br>
-                  <input type="checkbox" name="Public" value="public"> Public Post <br><br><br>
-
-						      <input type="submit" value="Create Poll" name="Create Poll" class="btn btn-block btn-primary" />
-						      <div class="module">
-						    </form>
-
-                <form action="poll_id.php">
-								    <button class="btn btn-gradient" type="submit">View My Polls</button>
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-
-	</div>
-
-	<div class="gototop js-top">
-		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
-	</div>
-
-	<!-- jQuery -->
-	<script src="js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
-	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
-
-	<!-- Main -->
-	<script src="js/main.js"></script>
-
-	</body>
+        </ul>
+      </div>
+      <!-- ################################################################################################ -->
+    </div>
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+  </main>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="../layout/scripts/jquery.min.js"></script>
+<script src="../layout/scripts/jquery.backtotop.js"></script>
+<script src="../layout/scripts/jquery.mobilemenu.js"></script>
+</body>
 </html>
