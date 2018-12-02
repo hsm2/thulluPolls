71d4632a201c51c83f172ced85b764d1d3cc6099
sjain0721 <surabhijain0721@gmail.com>
2018-12-02 12:07:31 -0600

    hello

--- participate.php
+++ participate.php
@@ -26,7 +26,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
   if (isset($_POST['comment'])) {
     echo "hello";
     $_SESSION['flag'] = TRUE;
-    $poll_id = $_SESSION['comment_id']
+    $poll_id = $_SESSION['comment_id'];
     $comment_id = uniqid();
     $user = $_SESSION['username'];
     $comment_text = $mysqli->real_escape_string($_POST['comments']);
