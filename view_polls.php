35a024b5f5e090d41dad0fda674cfacf487c90c6
sjain0721 <surabhijain0721@gmail.com>
2018-12-02 14:04:33 -0600

    hello

--- view_polls.php
+++ view_polls.php
@@ -30,6 +30,7 @@ echo $_SESSION['username']
         <li><a class="drop" href="#">Pages</a>
           <ul>
             <li><a href="participate.php">Participate in Polls</a></li>
+            <li><a href="private_id.php">Participate in Private Polls</a></li>
             <li><a href="view_polls.php">View Your Polls</a></li>
             <li><a href="create.php">Create Polls</a></li>
           </ul>
