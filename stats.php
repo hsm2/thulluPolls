ef2efca7dd3606b93530990e9db5e61a0a0e1ded
sjain0721 <surabhijain0721@gmail.com>
2018-12-01 18:08:34 -0600

    hello

--- stats.php
+++ stats.php
@@ -20,51 +20,31 @@ if($result->num_rows > 0) {
   }
 }
 
-echo $options[1];
-echo "not";
 
 ?>
  <html>
    <head>
      <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
      <script type="text/javascript" src="require.js">
-//       console.log("5");
-//       google.charts.load('current', {'packages':['corechart']});
-//       google.charts.setOnLoadCallback(drawChart);
-//       console.log("8");
-//       var mysql = require('mysql');
-//
-//
-//       con.connect(function(err) {
-//         if (err) throw err;
-//         console.log("Connected!");
-//       });
-//
-//       con.connect(function(err) {
-//         if (err) throw err;
-//         con.query("SELECT * FROM Options", function (err, result, fields) {
-//           if (err) throw err;
-//           console.log(result);
-//         });
-//       });
-//
-//
-//       function drawChart() {
-//         var data = google.visualization.arrayToDataTable([
-//           ['Task', 'Hours per Day'],
-//           ['Work',     11],
-//           ['Eat',      2],
-//           ['Commute',  2],
-//           ['Watch TV', 2],
-//           ['Sleep',    7]
-//         ]);
-//
-//         var options = {
-//           title: 'My Daily Activities'
-//         };
-//
-//         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-//
-//         chart.draw(data, options);
-//       }
-//     </script>
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>
