14056bd1fd4ed93b0e345ec804c97c8aae1f8c7b
sjain0721 <surabhijain0721@gmail.com>
2018-12-01 18:26:43 -0600

    hello

--- stats.php
+++ stats.php
@@ -44,11 +44,11 @@ if($result->num_rows > 0) {
 
 
        var data = google.visualization.arrayToDataTable([
-         ['Task', 'Hours per Day'],
-         ["<?php echo $options[0] ?>",  3],
-         ["<?php echo $options[1]?>",    2],
-          ["<?php echo $options[2]?>",  4],
-          ["<?php echo $options[3]?>", 1]
+        ['Task', 'Hours per Day'],
+        ["<?php echo $options[0] ?>", <?php echo $votes[0] ?>],
+        ["<?php echo $options[1]?>",  2],
+        ["<?php echo $options[2]?>",  4],
+        ["<?php echo $options[3]?>",  1]
 
        ]);
 
