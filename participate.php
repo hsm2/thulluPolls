199e9460f5e0c769af4b3d83855545a72f2c1b46
sjain0721 <surabhijain0721@gmail.com>
2018-12-01 21:47:11 -0600

    hello

--- participate.php
+++ participate.php
@@ -112,7 +112,6 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 $mysqli->close();
 ?>
 
-
 <html lang="">
 <head>
 <title>Thullu Polls</title>
@@ -147,89 +146,98 @@ $mysqli->close();
     <!-- ################################################################################################ -->
   </header>
 </div>
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
+
 <div class="wrapper bgded overlay" style="background-image:url('images/Green.html');">
-  <div id="pageintro" class="hoc clear">
-    <!-- ################################################################################################ -->
-      <?php
-      $_SESSION['message'] = '';
-      $mysqli = new mysqli("127.0.0.1", "thullupolls_root", "Surabhiharish", "thullupolls_thullupolls");
+  <html>
+  <head><meta http-equiv="Content-Type" content="text/html; charset=us-ascii">
+  	<title></title>
+  	<link rel="stylesheet" href="w3.css">
+  </head>
 
-      $owner = $_SESSION['username'];
-      $sql = "SELECT * FROM Poll WHERE visibility = 'public' and owner <> '$owner'";
-      $result = $mysqli->query($sql);
+  <html>
+  	<head>
+  	<meta charset="utf-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  	<title>Thullu Polls &mdash; Share Your Opinions</title>
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<meta name="description" content="Free HTML5 Website Template by gettemplates.co" />
+  	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+  	<meta name="author" content="gettemplates.co" />
 
-      ?>
-      <div id="gtco-project">
-        <div class="container">
-          <div class="row row-pb-md">
+    	<!-- Facebook and Twitter integration -->
+  	<meta property="og:title" content=""/>
+  	<meta property="og:image" content=""/>
+  	<meta property="og:url" content=""/>
+  	<meta property="og:site_name" content=""/>
+  	<meta property="og:description" content=""/>
+  	<meta name="twitter:title" content="" />
+  	<meta name="twitter:image" content="" />
+  	<meta name="twitter:url" content="" />
+  	<meta name="twitter:card" content="" />
 
-            <script src="js/jquery.min.js"></script>
-          	<!-- jQuery Easing -->
-          	<script src="js/jquery.easing.1.3.js"></script>
-          	<!-- Bootstrap -->
-          	<script src="js/bootstrap.min.js"></script>
-          	<!-- Waypoints -->
-          	<script src="js/jquery.waypoints.min.js"></script>
+  	<!-- <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400|Montserrat:400,700" rel="stylesheet"> -->
 
-          	<!-- Main -->
-          	<script src="js/main.js"></script>
-      <?php
+  	<link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet">
 
-      if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-          ?>
-          <div class="col-md-4 col-sm-4 col-xs-6 fh5co-project animate-box">
-              <h3><b><font size = "4"> <?php echo $row['poll_name'] ?> </font></b></h3>
-              <h3><b><font size = "4"><?php echo $row['question'] ?></font> </b> </h3>
+  	<!-- Animate.css -->
+  	<link rel="stylesheet" href="css/animate.css">
+  	<!-- Icomoon Icon Fonts-->
+  	<link rel="stylesheet" href="css/icomoon.css">
+  	<!-- Themify Icons-->
+  	<link rel="stylesheet" href="css/themify-icons.css">
+  	<!-- Bootstrap  -->
+  	<link rel="stylesheet" href="css/bootstrap.css">
+  	<!-- Theme style  -->
+  	<link rel="stylesheet" href="css/style.css">
 
-              <h3><b><font size = "4">Options: </font></b></h3><?php
-                  $id = $row['id'];
-                  $sq = "SELECT * FROM Options WHERE poll_id = '$id'";
-                  $result1 = $mysqli->query($sq);
-                  if($result1->num_rows > 0) {
-                    while($row1 = $result1->fetch_assoc()) {
-                      ?> <h3> <?php echo $row1['option_num']?> : <?php echo$row1['option_name'] ?> </h3> <?php
-                    }
-                  }
-                  ?>
+  	<!-- Modernizr JS -->
+  	<script src="js/modernizr-2.6.2.min.js"></script>
+  	<!-- FOR IE9 below -->
+  	<!--[if lt IE 9]>
+  	<script src="js/respond.min.js"></script>
+  	<![endif]-->
 
-                  <form class="form" action="#" method="post" enctype="multipart/form-data" autocomplete="off" onsubmit="<?php $_SESSION['poll_id'] = $id?>">
-                    <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
-                    <input type="text" placeholder="Option Number" name="number" required />
-                    <input type="checkbox" name="Like" value="like"> Like <br><br><br>
-                    <input type="submit" value="verify" name=<?= $id ?> class="btn btn-block btn-primary" onClick = "<?= $_SESSION['comment'] = "no"?>"/>
-                  </center>
-                    <div class="module"> </div>
-                  </form>
-                  <h2><font size = "3">Comments:</font></h2>
-                  <div class="myBox">
-                    <?php
-                        $id = $_SESSION['poll_id'];
-                        $sq = "SELECT user_id, comment_text FROM Comments WHERE poll_id = '$id'";
-                        $res = $mysqli->query($sq);
-                        if($res->num_rows > 0) {
-                          while($row1 = $res->fetch_assoc()) {
-                            ?> <h5> <b><?php echo $row1['user_id']?><b>  : <?php echo $row1['comment_text'] ?> </h5> <?php
-                          }
-                        }
-                        ?>
-                    </div>
-                    <form class="form" action="#" method="post" enctype="multipart/form-data" autocomplete="off" onsubmit="<?php $_SESSION['poll_id'] = $id?>">
-                      <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
-                      <input type="text" placeholder="Comment on this poll" name="comment" required />
-                      <input type="submit" value="comment" name=<?= $id ?> class="btn"/>
-                    </center>
-                      <div class="module"> </div>
-                    </form>
-                  <a class="topnav" href="stats.php" title="Homepage" onClick = "<?php $_SESSION['poll_id_stats'] = $id?>"><font color="red">View Statistics of this poll.</font></a>
-                  <?php
-              ?>
-          </div>
-      	<!-- jQuery -->
-      	<script src="js/jquery.min.js"></script>
+  	</head>
+  	<body>
+      <div class="dtc animate-box">
+        <center>
+        <h2 class="gradient-text">Public Polls</h2>
+        <a class="topnav" href="welcome.php" title="Homepage">Home</a><br>
+        <a class="topnav" href="private_id.php" title="Private Polls">Private Polls</a>
+      </div>
+    </body>
+
+
+  	<!-- jQuery -->
+  	<script src="js/jquery.min.js"></script>
+  	<!-- jQuery Easing -->
+  	<script src="js/jquery.easing.1.3.js"></script>
+  	<!-- Bootstrap -->
+  	<script src="js/bootstrap.min.js"></script>
+  	<!-- Waypoints -->
+  	<script src="js/jquery.waypoints.min.js"></script>
+
+  	<!-- Main -->
+  	<script src="js/main.js"></script>
+
+  	</body>
+  </html>
+
+
+  <?php
+  $_SESSION['message'] = '';
+  $mysqli = new mysqli("127.0.0.1", "thullupolls_root", "Surabhiharish", "thullupolls_thullupolls");
+
+  $owner = $_SESSION['username'];
+  $sql = "SELECT * FROM Poll WHERE visibility = 'public' and owner <> '$owner'";
+  $result = $mysqli->query($sql);
+
+  ?>
+  <div id="gtco-project">
+    <div class="container">
+      <div class="row row-pb-md">
+
+        <script src="js/jquery.min.js"></script>
       	<!-- jQuery Easing -->
       	<script src="js/jquery.easing.1.3.js"></script>
       	<!-- Bootstrap -->
@@ -239,25 +247,86 @@ $mysqli->close();
 
       	<!-- Main -->
       	<script src="js/main.js"></script>
-          <?php
-        }
-      }
-      else {
-        echo "0 results";
-      }
-      ?>
-      </div>
+  <?php
 
-    <!-- ################################################################################################ -->
+  if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      ?>
+      <div class="col-md-4 col-sm-4 col-xs-6 fh5co-project animate-box">
+          <h3><b><font size = "4"> <?php echo $row['poll_name'] ?> </font></b></h3>
+          <h3><b><font size = "4"><?php echo $row['question'] ?></font> </b> </h3>
+
+          <h3><b><font size = "4">Options: </font></b></h3><?php
+              $id = $row['id'];
+              $sq = "SELECT * FROM Options WHERE poll_id = '$id'";
+              $result1 = $mysqli->query($sq);
+              if($result1->num_rows > 0) {
+                while($row1 = $result1->fetch_assoc()) {
+                  ?> <h3> <?php echo $row1['option_num']?> : <?php echo$row1['option_name'] ?> </h3> <?php
+                }
+              }
+              ?>
+
+              <form class="form" action="#" method="post" enctype="multipart/form-data" autocomplete="off" onsubmit="<?php $_SESSION['poll_id'] = $id?>">
+                <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
+                <input type="text" placeholder="Option Number" name="number" required />
+                <input type="checkbox" name="Like" value="like"> Like <br><br><br>
+                <input type="submit" value="verify" name=<?= $id ?> class="btn btn-block btn-primary" onClick = "<?= $_SESSION['comment'] = "no"?>"/>
+              </center>
+                <div class="module"> </div>
+              </form>
+              <h2><font size = "3">Comments:</font></h2>
+              <div class="myBox">
+                <?php
+                    $id = $_SESSION['poll_id'];
+                    $sq = "SELECT user_id, comment_text FROM Comments WHERE poll_id = '$id'";
+                    $res = $mysqli->query($sq);
+                    if($res->num_rows > 0) {
+                      while($row1 = $res->fetch_assoc()) {
+                        ?> <h5> <b><?php echo $row1['user_id']?><b>  : <?php echo $row1['comment_text'] ?> </h5> <?php
+                      }
+                    }
+                    ?>
+                </div>
+                <form class="form" action="#" method="post" enctype="multipart/form-data" autocomplete="off" onsubmit="<?php $_SESSION['poll_id'] = $id?>">
+                  <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
+                  <input type="text" placeholder="Comment on this poll" name="comment" required />
+                  <input type="submit" value="comment" name=<?= $id ?> class="btn"/>
+                </center>
+                  <div class="module"> </div>
+                </form>
+              <a class="topnav" href="stats.php" title="Homepage" onClick = "<?php $_SESSION['poll_id_stats'] = $id?>"><font color="red">View Statistics of this poll.</font></a>
+              <?php
+          ?>
+      </div>
+  	<!-- jQuery -->
+  	<script src="js/jquery.min.js"></script>
+  	<!-- jQuery Easing -->
+  	<script src="js/jquery.easing.1.3.js"></script>
+  	<!-- Bootstrap -->
+  	<script src="js/bootstrap.min.js"></script>
+  	<!-- Waypoints -->
+  	<script src="js/jquery.waypoints.min.js"></script>
+
+  	<!-- Main -->
+  	<script src="js/main.js"></script>
+      <?php
+    }
+  }
+  else {
+    echo "0 results";
+  }
+  ?>
   </div>
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <a href="#" class="btn btn-gradient gtco-load"><i class="ti-reload"></i> load more...</a>
+    </div>
+  </div>
+  </div>
+  </div>
+  <?php
+  ?>
 </div>
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- JAVASCRIPTS -->
-<script src="layout/scripts/jquery.min.js"></script>
-<script src="layout/scripts/jquery.backtotop.js"></script>
-<script src="layout/scripts/jquery.mobilemenu.js"></script>
-<script src="layout/scripts/jquery.flexslider-min.js"></script>
-</body>
+
 </html>
