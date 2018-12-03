b52f8d00b9ff776fedd03c0a4895c4ae93566f5f
sjain0721 <surabhijain0721@gmail.com>
2018-12-03 07:44:17 -0600

    hello

--- stats.php
+++ stats.php
@@ -11,6 +11,65 @@ echo $_SESSION['username'];
 $_SESSION['flag'] = TRUE;
 ?>
 
+<?php
+$mysqli = new mysqli("127.0.0.1", "thullupolls_root", "Surabhiharish", "thullupolls_thullupolls");
+$id = $_SESSION['poll_id_stats'];
+
+$sql1 = "SELECT option_name, total_votes FROM Options WHERE poll_id = '$id' GROUP BY option_num";
+$result1 = $mysqli->query($sql1);
+
+$sql2 = "SELECT o.option_num, o.option_name, ov.timestamp FROM OptionVoters ov, Options o WHERE (ov.poll_id = '$id' AND o.poll_id = ov.poll_id AND o.option_num = ov.option_num) ORDER BY timestamp";
+$result2 = $mysqli->query($sql2);
+
+$sql3 = "SELECT poll_name, question FROM Poll WHERE id = '$id'";
+$result3 = $mysqli->query($sql3);
+
+$options = array();
+$votes = array();
+
+if($result1->num_rows > 0) {
+  while ($row = $result1->fetch_assoc()) {
+      array_push($options, $row['option_name']);
+      array_push($votes, $row['total_votes']);
+  }
+}
+
+$val1 = 0;
+$val2 = 0;
+$val3 = 0;
+$val4 = 0;
+
+$data = array();
+
+if ($result2->num_rows > 0) {
+  while ($row = $result2->fetch_assoc()) {
+    $t = $row['timestamp'];
+    $name = $row['option_name'];
+    $num = $row['option_num'];
+    $date = new DateTime("@$t");
+    if ($num == 1) {
+      $val1 = $val1 + 1;
+    }
+    else if ($num == 2) {
+      $val2 = $val2 + 1;
+    }
+    else if ($num == 3) {
+      $val3 = $val3 + 1;
+    }
+    else {
+      $val4 = $val4 + 1;
+    }
+    $data[] = array($t, $val1, $val2, $val3, $val4);
+  }
+}
+else {
+  ?> <h2> <?php echo "ahhhhh no values"?> </h2> <?php
+}
+
+$poll = $result3->fetch_assoc();
+
+?>
+
 
 <html lang="">
 <head>
@@ -129,7 +188,38 @@ $_SESSION['flag'] = TRUE;
                 <time datetime="2045-04-06T08:15+00:00">Friday, 6<sup>th</sup> April 2045 @08:15:00</time>
               </header>
               <div class="comcont">
-                <p>This is an example of a comment made on a post. You can either edit the comment, delete the comment or reply to the comment. Use this as a place to respond to the post or to share what you are thinking.</p>
+                <p>
+                  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                  <script type="text/javascript">
+                    google.charts.load('current', {'packages':['corechart']});
+                    google.charts.setOnLoadCallback(drawChart);
+
+                    function drawChart() {
+                      var data = google.visualization.arrayToDataTable([
+                       ['Task', 'Hours per Day'],
+                       ["<?php echo $options[0] ?>", <?php echo $votes[0] ?>],
+                       ["<?php echo $options[1]?>",  <?php echo $votes[1] ?>],
+                       ["<?php echo $options[2]?>",  <?php echo $votes[2] ?>],
+                       ["<?php echo $options[3]?>",  <?php echo $votes[3] ?>]
+
+                      ]);
+
+                      var options = {
+                        title: 'Statistics'
+                      };
+
+                      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+                      chart.draw(data, options);
+                    }
+
+                  </script>
+                  <body>
+                    <div id="piechart" style="width: 900px; height: 500px;"></div>
+                    <div id="chartContainer" style="height: 300px; width: 100%;"></div>
+                    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+                  </body>
+                </p>
               </div>
             </article>
           </li>
@@ -232,7 +322,7 @@ $_SESSION['flag'] = TRUE;
       </div>
       <div class="sdb_holder">
         <article>
-          <h6>Lorem ipsum dolor</h6>
+          <h6>Pie chart of poll options:</h6>
           <p>Nuncsed sed conseque a at quismodo tris mauristibus sed habiturpiscinia sed.</p>
           <ul>
             <li><a href="#">Lorem ipsum dolor sit</a></li>
