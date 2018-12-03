6676a793632490624601566bc0fc5f3fbd97d422
sjain0721 <surabhijain0721@gmail.com>
2018-12-03 08:35:07 -0600

    hello

--- participate.php
+++ participate.php
@@ -188,19 +188,6 @@ $mysqli->close();
         <p> <p> <b> <?php echo $max_p['poll_name'];?> </b>by <?php echo $max_p['name']?> with a total of <?php echo $max_p['total_votes'];?> likes! </p>
 
       </div>
-      <div class="sdb_holder">
-        <article>
-          <h6>Lorem ipsum dolor</h6>
-          <p>Nuncsed sed conseque a at quismodo tris mauristibus sed habiturpiscinia sed.</p>
-          <ul>
-            <li><a href="#">Lorem ipsum dolor sit</a></li>
-            <li>Etiam vel sapien et</li>
-            <li><a href="#">Etiam vel sapien et</a></li>
-          </ul>
-          <p>Nuncsed sed conseque a at quismodo tris mauristibus sed habiturpiscinia sed. Condimentumsantincidunt dui mattis magna intesque purus orci augue lor nibh.</p>
-          <p class="more"><a href="#">Continue Reading &raquo;</a></p>
-        </article>
-      </div>
       <!-- ################################################################################################ -->
     </div>
     <!-- ################################################################################################ -->
