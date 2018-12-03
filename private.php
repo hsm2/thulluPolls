0bf2359bfafff089fba1bd8b945e4033a2d128e1
sjain0721 <surabhijain0721@gmail.com>
2018-12-02 21:45:08 -0600

    hello

--- private.php
+++ private.php
@@ -225,7 +225,7 @@ $mysqli->close();
                       </form >
 
                       <form class="form" action="#" method="post" enctype="multipart/form-data" autocomplete="off" name="<?php echo $row['id'] ?>" onsubmit="">
-                        <input type="submit" value="View Statistics" name="view_stats"  class="btn" style="font-size:8pt;color:red;background-color:white;border:2px solid #FFFFFF;padding:3px" />
+                        <input type="submit" value="View Statistics" name="view_stats"  class="btn" style="font-size:8pt;color:#C8273D;background-color:white;border:2px solid #FFFFFF;padding:3px" />
                         <input type="hidden" name="id" value= "<?php echo $row['id'] ?>"/>
                       </form>
 
