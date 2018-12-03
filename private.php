acdc9ead3b93e66514e5390a3c27039673b90f00
sjain0721 <surabhijain0721@gmail.com>
2018-12-02 18:14:52 -0600

    hello

--- private.php
+++ private.php
@@ -44,11 +44,9 @@ $mysqli = new mysqli("127.0.0.1", "thullupolls_root", "Surabhiharish", "thullupo
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
   if (isset($_POST['view_stats'])) {
-    echo "helllllllllo";
     $_SESSION['poll_id_stats'] = $mysqli->real_escape_string($_POST['id']);
-    echo "hello";
     header("Location:stats.php");
-    echo "ggskdjflks";
+    echo "shouldn't ve here";
     ob_flush();
   }
 
