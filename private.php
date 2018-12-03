31e0ab2f03eca97d78f1d36b96d2ca95f663194b
sjain0721 <surabhijain0721@gmail.com>
2018-12-02 18:35:12 -0600

    hello

--- private.php
+++ private.php
@@ -1,34 +1,3 @@
-<style>
-.myBox {
-border: none;
-padding: 5px;
-font: 12px/14px sans-serif;
-width: 500px;
-height: 100px;
-overflow: scroll;
-}
-
-#container div{
-    display:inline-block;
-    width:130px;
-}
-
-div.wpforms-container-full .wpforms-form button[type=submit] {
-
-color: #0099CC; /* Text color */
-
-background-color: transparent; /* Remove background color */
-
-border: 2px solid #0099CC; /* Border thickness, line style, and color */
-
-border-radius: 5px; /* Adds curve to border corners */
-
-text-transform: uppercase; /* Make letters uppercase */
-
-}
-
-</style>
-
 <?php
  ob_start();  //begin buffering the output
 ?>
