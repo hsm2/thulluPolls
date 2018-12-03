b2ee56e97e72e99aa377a4b5b11e827e312b1891
sjain0721 <surabhijain0721@gmail.com>
2018-12-02 20:39:54 -0600

    hello

--- stats.php
+++ stats.php
@@ -7,7 +7,7 @@
 <?php
 session_start();
 $mysqli = new mysqli("127.0.0.1", "thullupolls_root", "Surabhiharish", "thullupolls_thullupolls");
-$id = '5c030de095690';
+$id =$_SESSION['poll_id_stats'];
 
 $sql1 = "SELECT option_name, total_votes FROM Options WHERE poll_id = '$id' GROUP BY option_num";
 $result1 = $mysqli->query($sql1);
