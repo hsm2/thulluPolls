51b5e0e3978892ccc58d1f2da790c2dee0bbd111
sjain0721 <surabhijain0721@gmail.com>
2018-12-02 21:08:37 -0600

    hello

--- participate.php
+++ participate.php
@@ -267,7 +267,7 @@ $mysqli->close();
                       <form class="form" action="#" method="post" enctype="multipart/form-data" autocomplete="off" name="<?php echo $row['id'] ?>" onsubmit="">
                         <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
                         <input type="text" placeholder="Option Number" name="number" required />
-                        <input type="checkbox" name="Like" value="like"> Like <br><br><br>
+                        <input type="checkbox" name="Like" value="like"> Like: <?php $row['total_likes'] ?><br><br><br>
                         <input type="submit" value="verify" name="vote" class="btn btn-block btn-primary"/>
                         <input  type="hidden" name="id" value= "<?php echo $row['id'] ?>" />
                       </center>
