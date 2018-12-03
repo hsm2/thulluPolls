ff7914bc7dbbfcf609323cbe1b63a29de57f6c1c
sjain0721 <surabhijain0721@gmail.com>
2018-12-03 08:15:40 -0600

    hello

--- stats.php
+++ stats.php
@@ -177,7 +177,7 @@ $poll = $result3->fetch_assoc();
 
                   </script>
                   <body>
-                    <div id="piechart" style="width: 300px; height: 200px;"></div>
+                    <div id="piechart" style="width: 500px; height: 300px;"></div>
                     <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
                   </body>
                 </p>
